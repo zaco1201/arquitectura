67802612025f3330d9327adbb52efdd5fb9d87e1
zaco1201 <sebastiansaborioc@gmail.com>
2020-03-12 18:11:05 -0600

    Subo tarea de arqui

---
+++ Tarea_IDA PRO/sumas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+
+    printf( "\n   Introduzca el primer numero (entero): " );
+    scanf( "%d", &n1 );
+
+    printf( "\n   Introduzca el segundo numero (entero): " );
+    scanf( "%d", &n2 );
+
+    printf( "\n   \t La suma es: %d \n", n1 + n2 );
+
+    return 0;
+}
